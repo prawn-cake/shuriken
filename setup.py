029c189416e3231867ee7ea65983426319183bf0
Maksim Ekimovskii <maxi@168.estate>
2014-11-25 21:46:22 +0800

    Added MANIFEST.in, fixed setup.py, install requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from distutils.core import setup
 from pip.req import parse_requirements
 import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 install_reqs = parse_requirements('/'.join([current_dir, 'requirements.txt']))
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='shuriken',
-    version='0.1',
+    version='0.1.2',
     packages=['shuriken'],
-    package_data={
-        '': ['*.txt', '*.rst']
-    },
     url='https://github.com/prawn-cake/shuriken.git',
     license='MIT',
     author='prawn-cake',
     author_email='ekimovsky.maksim@gmail.com',
     description='Shuriken is a monitoring agent which allows to do passive checks for Shinken monitoring system vie mod-ws-arbiter.',
-    install_requires=install_reqs
+    install_requires=reqs
 )
